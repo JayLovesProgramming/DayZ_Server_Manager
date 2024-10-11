5ce4fb478a0814d835655342136c8c82224606e9
Jay <116266395+JayLovesProgramming@users.noreply.github.com>
2024-10-11 18:42:27 +0100

    Update genmods.py
    Read from config.json

--- genmods.py
+++ genmods.py
@@ -1,36 +1,29 @@
 import os
+import json
 
-def find_folders_with_at_symbol(directory, output_file, ignore_file, priority_mods):
-    # List to hold folder names
+def find_folders_with_at_symbol(mods_dir, output_mods_txt_file_dir, ignore_txt_file_dir, priority_mods):
     folders_with_at = []
     ignored_folders = []
-    # Read ignore.txt to get a list of folders to ignore
-    if os.path.exists(ignore_file):
-        with open(ignore_file, 'r') as f:
+    if os.path.exists(ignore_txt_file_dir):
+        with open(ignore_txt_file_dir, 'r') as f:
             ignored_folders = [line.strip() for line in f.readlines()]
-    # Walk through the directory and its subdirectories
-    for _, dirs, _ in os.walk(directory):
+    for _, dirs, _ in os.walk(mods_dir):
         for dir_name in dirs:
             if dir_name.startswith('@') and dir_name not in ignored_folders:
                 folders_with_at.append(dir_name)
-    # Remove any priority mods that were found to avoid duplicates
     folders_with_at = [mod for mod in folders_with_at if mod not in priority_mods]
-    # Combine priority mods with the rest of the mods
     sorted_mods = priority_mods + folders_with_at
-    # Write the folder names to the specified output text file
-    with open(output_file, 'w') as f:
+    with open(output_mods_txt_file_dir, 'w') as f:
         for folder in sorted_mods:
             f.write(folder + '\n')
     print(f"Found {len(folders_with_at)} folders starting with '@' (excluding ignored ones).")
-    # print(f"Mods with priority: {', '.join(priority_mods)}")
-    # print(f"Saved to {output_file}.")
 
 if __name__ == '__main__':
-    directory = r"C:\DayZServer\resources\Utils"  # Use the current working directory
-    output_file_path = os.path.join(directory, 'mods.txt')
-    ignore_file_path = os.path.join(directory, 'ignore.txt')
-    
-    # List of priority mods (these will be listed first in the output file)
-    priority_mods = ["@CF", "@Dabs Framework", "@VPPAdminTools", '@DayZ-Expansion-Bundle', "@DayZ-Expansion-Licensed", "@DayZ-Expansion-Animations", "@DayZ Editor Loader", "@HMB", "@MuchFramework", "@MuchStuffPack"]  # Example priority list
-    
-    find_folders_with_at_symbol(r"C:\DayZServer\resources", output_file_path, ignore_file_path, priority_mods)
+    config_file = 'Utils/config.json'
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+    mods_dir = config['mods_dir']
+    output_mods_txt_file_dir = os.path.join(mods_dir, config['output_mods_txt_file_dir'])
+    ignore_txt_file_dir = os.path.join(mods_dir, config['ignore_txt_file_dir'])
+    priority_mods = config['priority_mods']
+    find_folders_with_at_symbol(mods_dir, output_mods_txt_file_dir, ignore_txt_file_dir, priority_mods)
